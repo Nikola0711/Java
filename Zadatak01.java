0f8518270784184b3e94f4e13079a16ec75cca09
Nikola0711 <57724179+Nikola0711@users.noreply.github.com>
2020-01-13 04:11:53 +0100

    Create Zadatak01.java

---
+++ Zadatak01.java
@@ -0,0 +1,60 @@
+# Java
+
+package Zadaci;
+
+import javax.swing.JOptionPane;
+
+public class Zadatak01 {
+
+	public static void main(String[] args) {
+		
+		int a=Integer.parseInt(JOptionPane.showInputDialog("Unesite broj"));
+		int b=Integer.parseInt(JOptionPane.showInputDialog("Unesite broj"));
+		int c=Integer.parseInt(JOptionPane.showInputDialog("Unesite broj"));
+		int d=Integer.parseInt(JOptionPane.showInputDialog("Unesite broj"));
+		int e=Integer.parseInt(JOptionPane.showInputDialog("Unesite broj"));
+		int f=Integer.parseInt(JOptionPane.showInputDialog("Unesite broj"));
+		int g=Integer.parseInt(JOptionPane.showInputDialog("Unesite broj"));
+		int h=Integer.parseInt(JOptionPane.showInputDialog("Unesite broj"));
+		int i=Integer.parseInt(JOptionPane.showInputDialog("Unesite broj"));
+		int j=Integer.parseInt(JOptionPane.showInputDialog("Unesite broj"));
+		int k=Integer.parseInt(JOptionPane.showInputDialog("Unesite broj"));
+		int l=Integer.parseInt(JOptionPane.showInputDialog("Unesite broj"));
+		int m=Integer.parseInt(JOptionPane.showInputDialog("Unesite broj"));
+		int n=Integer.parseInt(JOptionPane.showInputDialog("Unesite broj"));
+		int o=Integer.parseInt(JOptionPane.showInputDialog("Unesite broj"));
+		int p=Integer.parseInt(JOptionPane.showInputDialog("Unesite broj"));
+		int r=Integer.parseInt(JOptionPane.showInputDialog("Unesite broj"));
+		int s=Integer.parseInt(JOptionPane.showInputDialog("Unesite broj"));
+		int t=Integer.parseInt(JOptionPane.showInputDialog("Unesite broj"));
+		int u=Integer.parseInt(JOptionPane.showInputDialog("Unesite broj"));
+		int v=Integer.parseInt(JOptionPane.showInputDialog("Unesite broj"));
+		int z=Integer.parseInt(JOptionPane.showInputDialog("Unesite broj"));
+		int lj=Integer.parseInt(JOptionPane.showInputDialog("Unesite broj"));
+		int nj=Integer.parseInt(JOptionPane.showInputDialog("Unesite broj"));
+		
+		
+		int zbroj;
+		
+		zbroj=a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+r+s+t+u+v+z+lj+nj;
+		
+		System.out.println(zbroj);
+		
+		double max=0,min=999999;
+		
+		if(a>max) {
+			max=a;
+		}
+		if(a<min) {
+			min=a;
+		}
+		
+		System.out.println(max);
+		System.out.println(min);
+		
+		
+		
+
+	}
+
+}
